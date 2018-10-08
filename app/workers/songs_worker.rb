b54e04d0341b1cd881b7d7014a62a3207f130615
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-08 20:21:57 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -1,7 +1,7 @@
 class SongsWorker
   require 'csv'
   include Sidekiq::Worker
-     
+
   def perform(songs_file)
     CSV.foreach(songs_file, headers: true) do |song|
       Song.create(title: lead[0], artist_name: lead[1])
